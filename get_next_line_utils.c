e81d3cf2567b2cc7e57775c900bd1e462475dc5e
markoscomks <markoscomks@gmail.com>
2024-05-11 18:42:16 +0100

    Created and added utility functins

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -5,9 +5,77 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/11 14:45:35 by marsoare          #+#    #+#             */
-/*   Updated: 2024/05/11 14:45:56 by marsoare         ###   ########.fr       */
+/*   Created: 2024/05/11 18:20:28 by marsoare          #+#    #+#             */
+/*   Updated: 2024/05/11 18:39:03 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*endstr;
+	int		totlen;
+	int		i;
+
+	totlen = ft_strlen(s1) + ft_strlen(s2);
+	endstr = (char *)malloc(sizeof(char) * totlen + 1);
+	if (endstr == 0)
+		return (NULL);
+	i = 0;
+	while (*s1)
+	{
+		endstr[i] = *s1;
+		i++;
+		s1++;
+	}
+	while (*s2)
+	{
+		endstr[i] = *s2;
+		i++;
+		s2++;
+	}
+	endstr[i] = 0;
+	return (endstr);
+}
+
+int	ft_strlen(const char *s)
+{
+	int	size;
+
+	size = 0;
+	while (s[size])
+		size++;
+	return (size);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if ((char)c == *s)
+		return ((char *)s);
+	return (NULL);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*dup;
+	size_t	i;
+
+	dup = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!dup)
+		return (NULL);
+	i = 0;
+	while (i < ft_strlen(s))
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	dup[i] = 0;
+	return (dup);
+}
