fee1c39fb2985abc48deeb80dafbe246bf8df1ac
markoscomks <markoscomks@gmail.com>
2024-05-11 18:11:06 +0100

    Added base funcs

--- get_next_line.c
+++ get_next_line.c
@@ -5,14 +5,32 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/11 14:41:49 by marsoare          #+#    #+#             */
-/*   Updated: 2024/05/11 14:42:26 by marsoare         ###   ########.fr       */
+/*   Created: 2024/05/11 17:06:39 by marsoare          #+#    #+#             */
+/*   Updated: 2024/05/11 18:03:20 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*get_line(int fd, char *buf, char *backup);
+
 char	*get_next_line(int fd)
 {
+	char	*line;
+	char	*buf;
+	static char	*backup;
 
+	buf = malloc(sizeof(char) + (BUFFER_SIZE + 1));
+	if (!buf)
+		return (NULL);
+	line = get_line(fd, buf, backup);
+}
+
+char	*get_line(int fd, char *buf, char *backup)
+{
+	char	*line;
+	int		check_read;
+	check_read = read(fd, buf, BUFFER_SIZE);
+	line = buf;
+	return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -5,17 +5,22 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/11 14:42:35 by marsoare          #+#    #+#             */
-/*   Updated: 2024/05/11 14:44:50 by marsoare         ###   ########.fr       */
+/*   Created: 2024/05/11 17:07:48 by marsoare          #+#    #+#             */
+/*   Updated: 2024/05/11 18:04:43 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include "stdlib.h"
-#include "unistd.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+#endif
 
 char	*get_next_line(int fd);
+char	*get_line(int fd, char *buf, char *backup);
 
 #endif
