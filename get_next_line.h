956f6204e70a2a4bbe817adda1ff14e5e3871dbe
markoscomks <markoscomks@gmail.com>
2024-05-11 14:46:25 +0100

    created base proj

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/11 14:42:35 by marsoare          #+#    #+#             */
+/*   Updated: 2024/05/11 14:44:50 by marsoare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include "stdlib.h"
+#include "unistd.h"
+
+char	*get_next_line(int fd);
+
+#endif
