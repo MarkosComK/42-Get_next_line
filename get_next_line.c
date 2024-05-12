f006e7713d9366bbe98c5557e6ccb61f9c73155d
markoscomks <markoscomks@gmail.com>
2024-05-12 19:45:37 +0100

    Changed break params - Finding end of line inside loop

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 17:06:39 by marsoare          #+#    #+#             */
-/*   Updated: 2024/05/12 15:23:56 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/05/12 16:47:02 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,15 +35,19 @@ char	*get_line(int fd, char *buf, char *backup)
 
 	check_read = 1;
 	backup = ft_strdup("");
-	while (!(ft_strchr(backup, '\n')) || check_read < 0)
+	while (check_read != 0)
 	{
 		check_read = read(fd, buf, BUFFER_SIZE);
 		if (check_read < 0)
 			return (NULL);
+		if (check_read == 0)
+			break ;
 		buf[BUFFER_SIZE] = 0;
 		tmp = backup;
 		backup = ft_strjoin(tmp, buf);
 		free(tmp);
+		if (ft_strchr(backup, '\n'))
+			break ;
 	}
 	return (backup);
 }
