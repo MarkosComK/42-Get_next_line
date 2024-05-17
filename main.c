26e629668c777a5d1bbaa7a0359f4e7f8bb40a06
markoscomks <markoscomks@gmail.com>
2024-05-17 21:05:51 +0100

    Improved main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 16:37:34 by marsoare          #+#    #+#             */
-/*   Updated: 2024/05/17 15:08:58 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/05/17 20:24:19 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,3 +27,34 @@ int	main(void)
 	}
 	return (0);
 }
+/*
+#include "get_next_line_bonus.h"
+#include <stdio.h>
+#include <fcntl.h>
+
+int main(void)
+{
+    int     fd1;
+    int     fd2;
+    char    *file1 = "./texto.c";
+    char    *file2 = "./texto2.c";
+    char    *str;
+
+    fd1 = open(file1, O_RDONLY);
+    fd2 = open(file2, O_RDONLY);
+    str = "";
+    fd1 = 42;
+    fd2 = 42;
+    while (str)
+    {
+        str = get_next_line(fd1);
+        printf ("%s", str);
+        free(str);
+        str = get_next_line(fd2);
+        printf ("%s", str);
+        free(str);
+    }
+    close(fd1);
+    return (0);
+}
+*/
