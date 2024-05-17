4a929cf1e316e3d413bd1e434c0316e9f9314fd7
markoscomks <markoscomks@gmail.com>
2024-05-17 16:34:03 +0100

    corrected identation

--- get_next_line.h
+++ get_next_line.h
@@ -6,19 +6,19 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 17:07:48 by marsoare          #+#    #+#             */
-/*   Updated: 2024/05/17 15:45:47 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/05/17 16:33:42 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <stdlib.h>
-#include <unistd.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 char	*get_next_line(int fd);
 char	*get_read(int fd, char *backup);
